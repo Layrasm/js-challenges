44ae0eb6b46d05f158840672e6a892f33f323675
Layne Rasmussen <layrasm@gmail.com>
2020-09-09 11:12:19 -0600

    alphabet sorted

--- vowel.js
+++ vowel.js
@@ -2,19 +2,37 @@
 
 // 
 
-let vowels = [a,e,i,o,u]
+// let vowels = [a,e,i,o,u]
 
-function findVowels (str) {
-  let vowels = [a,e,i,o,u];
-  let count = 0;
+// function findVowels (str) {
+//   let vowels = 'a,e,i,o,u';
+//   let count = 0;
 
-  for(var i = 0; i < str.length; i++) 
-  {
-    if (vowels.indexOf(str[i] !== -1)
-   {
-     count =+1;
-   }
-  return count;
-}
+//   for(var i = 0; i < str.length; i++) 
+//   {
+//     if (vowels.indexOf(str[i]) !== -1)
+//    {
+//      count =+1;
+//    }
+//   }
+//   return count;
+// }
 
-console.log(findVowels("hello there"));
+// console.log(findVowels("hello there"));
+
+
+
+// James example
+// function findVowels(str){
+//   vowelCount = {a:0, e:0, i:0, o:0, u:0};
+//   // I want to go through each character and update object
+//   str.split("").foreach(function(thing){
+//     console.log(thing);
+//   });
+//   return vowelCount;
+// }
+
+// vowels = findVowels ("hello there");
+// console.log(vowels);
+
+console.log("hello");
